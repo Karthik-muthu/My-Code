63aa28c4a533eb3c728520c0979dee16d1dbc5c9
Karthik-muthu <karthikmuthuramalingam99@gmail.com>
2025-02-04 10:30:18 +0530

    Add files via upload

--- myassignments/Palindrome.java
+++ myassignments/Palindrome.java
@@ -1,10 +1,32 @@
 package myassignments;
 
+
+import java.util.Scanner;
 public class Palindrome {
 
 	public static void main(String[] args) {
+
+		int number,orgNumber;
+		
+		System.out.println("Enter the number; ");
+		Scanner sc= new Scanner(System.in);
+		number= sc.nextInt();
+		orgNumber=number;
+		int reverseNumber=0;
+		while(number>0) {
+			reverseNumber =reverseNumber*10+number%10;
+			number=number/10;
+		}
+		if(orgNumber==reverseNumber) {
+			System.out.println(orgNumber+" is a palindrome");
+		}
+		else {
+			System.out.println(orgNumber+" is not a palindrome");
+		}
+	}
+		
 		
 
 	}
 
-}
+
